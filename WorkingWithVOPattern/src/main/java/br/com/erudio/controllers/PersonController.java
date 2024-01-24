bcbf0fd297da5d62e09eeb179994c89424ab60dd
João Dias <joaovitorelf@gmail.com>
2024-01-24 15:44:50 -0300

    implementing VO pattern with Dozer

---
+++ WorkingWithVOPattern/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,51 @@
+package br.com.erudio.controllers;
+
+import br.com.erudio.data.vo.v1.PersonVO;
+import br.com.erudio.model.Person;
+import br.com.erudio.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @GetMapping(value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonVO findById( @PathVariable(value = "id") Long id) throws Exception {
+        return service.findById(id);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<PersonVO> findAll() {
+        return service.findAll();
+    }
+
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonVO create(@RequestBody PersonVO person) throws Exception {
+        return service.create(person);
+    }
+
+    @PutMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public PersonVO update( @RequestBody PersonVO person) throws Exception {
+        return service.update(person);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) throws Exception {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
