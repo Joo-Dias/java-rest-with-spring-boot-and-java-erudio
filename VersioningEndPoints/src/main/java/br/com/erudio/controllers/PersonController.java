02e940cfb07f425bb9be53c08499a8e5f1e32784
João Dias <joaovitorelf@gmail.com>
2024-01-24 18:51:31 -0300

    versioning our API

--- VersioningEndPoints/src/main/java/br/com/erudio/controllers/PersonController.java
+++ VersioningEndPoints/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -1,7 +1,6 @@
 package br.com.erudio.controllers;
 
 import br.com.erudio.data.vo.v1.PersonVO;
-import br.com.erudio.data.vo.v2.PersonVOV2;
 import br.com.erudio.model.Person;
 import br.com.erudio.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,14 +33,6 @@ public class PersonController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     public PersonVO create(@RequestBody PersonVO person) throws Exception {
         return service.create(person);
-    }
-
-    @PostMapping(
-            value = "/v2",
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public PersonVOV2 createV2(@RequestBody PersonVOV2 person) throws Exception {
-        return service.createV2(person);
     }
 
     @PutMapping(
