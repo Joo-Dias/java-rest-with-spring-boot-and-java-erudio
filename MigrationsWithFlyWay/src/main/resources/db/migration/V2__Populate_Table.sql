3a5379d675fa670302feeb4528396c9c839c3d79
João Dias <joaovitorelf@gmail.com>
2024-01-24 20:23:40 -0300

    adding migrations support with FlyWay

---
+++ MigrationsWithFlyWay/src/main/resources/db/migration/V2__Populate_Table.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`)
+VALUES (1, 'São Paulo', 'João V', 'Male', 'S Dias'),
+       (2, 'Minas Gerais', 'Nando Moura', 'Male', 'Silva'),
+       (5, 'Java - Intellij', 'Value', 'Male', 'Object');
